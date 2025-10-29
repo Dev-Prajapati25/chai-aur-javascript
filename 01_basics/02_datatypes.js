fa95163f0d9e43fba659a59381c42c03f595adbe
Dev-Prajapati25 <pdev25214@gmail.com>
2025-10-29 23:45:48 +0530

    Added 02_datatypes.js

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict";   // Tells engine that code is written in newer js (not necessary to write this but it'll be considered a good practice)
+
+// alert("Message='Hello'")    //This throws error
+// alert() works in browser but not in nodejs
+
+//! Datatypes
+
+// number - int & float
+// bigint - for big integers
+// string
+// boolean - true/false
+// symbol - uniqueness
+
+// null - represents 'empty' value
+// undefined - represents value that is not defined
+
+console.log(typeof null);       //* 'object'
+console.log(typeof undefined);  //* 'undefined'
